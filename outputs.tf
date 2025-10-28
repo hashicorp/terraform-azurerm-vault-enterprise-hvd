ae217371e5b7e14a79d5cdbe65d886030890cc34
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-10-28 10:51:11 -0400

    Tweak outputs

--- outputs.tf
+++ outputs.tf
@@ -9,11 +9,15 @@ output "resource_group_name" {
   description = "Name of the Resource Group."
 }
 
+locals {
+  vault_lb_ip = var.lb_is_internal ? azurerm_lb.vault[0].private_ip_address : azurerm_public_ip.vault_lb[0].ip_address
+}
+
 #------------------------------------------------------------------------------
 # Vault
 #------------------------------------------------------------------------------
 output "load_balancer_ip" {
-  value       = azurerm_public_ip.vault_lb[0].ip_address
+  value       = local.vault_lb_ip
   description = "IP address of load balancer's frontend configuration"
 }
 
@@ -25,15 +29,15 @@ output "vault_server_private_ips" {
 output "vault_cli_config" {
   description = "Environment variables to configure the Vault CLI"
   value       = <<-EOF
-    %{ if var.create_lb == true ~}
-    export VAULT_ADDR=https://${azurerm_public_ip.vault_lb[0].ip_address}:8200
-    %{ else ~}
+    %{if var.create_lb == true~}
+    export VAULT_ADDR=https://${local.vault_lb_ip}:8200
+    %{else~}
     # No load balancer created; set VAULT_ADDR to the IPV4 address of any Vault instance
     export VAULT_ADDR=https://${data.azurerm_virtual_machine_scale_set.vault.instances[0].private_ip_address}:8200
-    %{ endif ~}
+    %{endif~}
     export VAULT_TLS_SERVER_NAME=${var.vault_fqdn}
-    %{ if var.vault_tls_ca_bundle_keyvault_secret_id != null ~}
+    %{if var.vault_tls_ca_bundle_keyvault_secret_id != null~}
     export VAULT_CACERT=<path/to/ca-certificate>
-    %{ endif ~}
+    %{endif~}
   EOF
 }
